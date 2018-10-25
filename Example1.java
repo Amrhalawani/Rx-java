654fa7e0237890704ad79b591e524e646760b42f
Amrhal <eng.amrhalawani@rocketmail.com>
2018-10-25 16:37:50 +0200

    just operator exp

--- Example1.java
+++ Example1.java
@@ -20,9 +20,9 @@ public class Example1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example1);
-        //http://www.hendiware.com/%D8%A7%D9%84%D9%80-rx-%D8%A8%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9-rx-java-%D8%A7%D9%84%D9%85%D9%83%D9%88%D9%86%D8%A7%D8%AA/
-
+        
         Test1();
+
 
     }
 
